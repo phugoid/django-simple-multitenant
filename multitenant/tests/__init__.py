c24062e8f6663e94837ee2da44010959ab7006b8
Daniel Romaniuk <daniel.romaniuk@gmail.com>
2012-01-23 09:49:57 -0500

    Set up test structure, with one test module for each module of the application.

---
+++ multitenant/tests/__init__.py
@@ -0,0 +1,4 @@
+from multitenant.tests.forms import *
+from multitenant.tests.middleware import *
+from multitenant.tests.models import *
+from multitenant.tests.utils import *
---
+++ multitenant/tests/forms.py
@@ -0,0 +1,3 @@
+from django.test import TestCase
+
+# TODO: Add form tests.
